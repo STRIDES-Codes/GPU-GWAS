26a02fd9fcb8bc1a4de8f5be163de77a3d6b8255
Kyle Kranen <kkranen@nvidia.com>
2020-11-07 19:03:05 +0000

    Basic annotation loading

--- gpugwas/io.py
+++ gpugwas/io.py
@@ -1,5 +1,10 @@
 """Module for loading data into dataframe."""
+import cudf
 
 def _vcf_to_df(vcf_file):
     """Function to load VCF into gwas dataframe."""
+    
+def _load_annotations(annotation_path, delimiter = '\t'):
+    """Function to load annotations into a Cudf (GPU accelerated) dataframe"""
+    return cudf.read_csv(annotation_path,delimiter=delimiter)
 

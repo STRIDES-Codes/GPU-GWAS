d68c03969f22bfd56d1b943d6e69fa269e5de3ca
Joyjit Daw <jdaw@nvidia.com>
2020-11-07 22:13:38 -0500

    [workflow] time after I/O

--- workflow.py
+++ workflow.py
@@ -10,17 +10,17 @@ parser.add_argument('--annotation_path', default = './data/1kg_annotations.txt')
 parser.add_argument('--workdir', default = './temp/')
 args = parser.parse_args()
 
-t0 = time.time()
-
 vcf_df = gwasio._load_vcf(args.vcf_path, info_keys=["AC", "AF"], format_keys=["GT"])
 
 
 ann_df = gwasio._load_annotations(args.annotation_path)
+
+# Start benchmarking after I/O
+t0 = time.time()
 
 vcf_df = gwasproc.filter_vcf(vcf_df)
 
 vcf_df = gwasproc.sample_vcf(vcf_df)
 
 sampled_vcf_df = gwasproc.annotate_samples(vcf_df, ann_df)
-
 print('Time Elapsed: {}'.format(time.time()- t0))

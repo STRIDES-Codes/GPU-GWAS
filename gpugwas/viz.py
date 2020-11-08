d4f40f79bce8c86c7557ec4289bbb8a7304a2ee0
Rajesh K Ilango <rilango@gmail.com>
2020-11-07 17:43:23 -0800

    Visualize QQ and Manhattan plot...
    Pre-req for this tool are
    pip install \
        dash \
        jupyter-dash \
        dash_bootstrap_components \
        dash_core_components \
        dash_html_components
    
    The script contains __main__ for testing and also has an example.

---
+++ gpugwas/viz.py
@@ -0,0 +1,160 @@
+#!/opt/conda/envs/rapids/bin/python
+"""
+Pre-reqs:
+---------
+/opt/conda/envs/rapids/bin/pip install \
+    dash \
+    jupyter-dash \
+    dash_bootstrap_components \
+    dash_core_components \
+    dash_html_components
+"""
+
+import cudf
+
+import plotly.graph_objects as go
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+from dash.dependencies import Input, Output, State, ALL
+
+
+EXT_STYLES = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
+
+
+class ManhattanPlot:
+
+    def __init__(self, scatter_spec, manhattan_spec):
+        self.app = dash.Dash( __name__, external_stylesheets=EXT_STYLES)
+
+        self.scatter_spec = scatter_spec
+        self.manhattan_spec = manhattan_spec
+
+        self.app.layout = self._construct()
+
+    def start(self, host=None, port=5000):
+        return self.app.run_server(
+            debug=False, use_reloader=False, host=host, port=port)
+
+    def _construct_scatter(self):
+
+        x_values = self.manhattan_spec['df'][self.manhattan_spec['x_axis']]
+        y_values = self.manhattan_spec['df'][self.manhattan_spec['y_axis']]
+
+        x_max = float(x_values.max())
+        y_max = float(y_values.max())
+
+        scatter_marker = go.Scattergl({
+                'x': self.manhattan_spec['df'][self.manhattan_spec['x_axis']].to_array(),
+                'y': y_values.to_array(),
+                'mode': 'markers',
+                'marker': {
+                    'size': 2,
+                    'color': '#406278',
+                },
+            })
+
+        scatter_line = go.Scattergl({
+                'x': [0, x_max],
+                'y': [0, y_max],
+                'mode': 'lines',
+                'line': {
+                    'width': 2,
+                    'color': 'orange',
+                },
+            })
+
+        scatter_fig = go.Figure(
+            data = [scatter_marker, scatter_line],
+            layout = {
+                'title': 'Q-Q Plot',
+                'showlegend': False,
+                'grid': {
+                    'columns' : 1,
+                },
+            })
+        return scatter_fig
+
+    def _construct_manhatten(self):
+        chroms = self.manhattan_spec['df'][self.manhattan_spec['group_by']].unique().to_array()
+
+        start_position = -0.5
+        scatter_traces = []
+
+        for chrom in chroms:
+            query = '%s == %s' % (self.manhattan_spec['group_by'], chrom)
+            cdf = self.manhattan_spec['df'].query(query)
+
+            x_array = cdf[self.manhattan_spec['x_axis']] + start_position 
+            scatter_trace = go.Scattergl({
+                    'x': x_array.to_array(),
+                    'y': cdf[self.manhattan_spec['y_axis']].to_array(),
+                    'name': 'Chromosome ' + str(chrom),
+                    'mode': 'markers',
+                    'marker': {
+                        'size': 2,
+                        'color': '#406278' if (start_position - 0.5) % 2 == 0 else '#e32636',
+                    },
+                })
+            scatter_traces.append(scatter_trace)
+            start_position += 1
+
+        manhattan_fig = go.Figure(
+            data = scatter_traces, 
+            layout = {
+                'title': 'GWAS Manhattan Plot',
+                'showlegend': False,
+                'grid': {
+                    'columns' : 1,
+                },
+                'xaxis': {
+                    'showgrid': False, 
+                    'gridwidth': 1, 
+                    'ticks': 'outside',
+                    'zeroline': False,
+                    'tickvals': [t for t in range(int(start_position + 0.5))],
+                    'ticktext': [str(t) for t in chroms],
+                }})
+        return manhattan_fig
+
+    def _construct(self):
+        manhattan_fig = self._construct_manhatten()
+        scatter_fig = self._construct_scatter()
+
+        layout = html.Div([
+            html.Div(
+                children=[
+                    dcc.Markdown(
+                        """
+                        **GWAS**
+                        """), 
+                    html.Div([dcc.Graph(id='scatter_fig', figure=scatter_fig),]),
+                    html.Div([dcc.Graph(id='manhattan-figure', figure=manhattan_fig),]),
+                ]),
+            ])
+
+        return layout
+
+
+def main():
+    df = cudf.read_csv('./data/data.csv')
+
+    scatter_spec = {}
+    scatter_spec['df'] = df
+    scatter_spec['x_axis'] = 'P'
+    scatter_spec['y_axis'] = 'ZSCORE'
+
+    manhattan_spec = {}
+    manhattan_spec['df'] = df
+    manhattan_spec['group_by'] = 'CHR'
+    manhattan_spec['x_axis'] = 'P'
+    manhattan_spec['y_axis'] = 'ZSCORE'
+
+    plot = ManhattanPlot(scatter_spec, manhattan_spec)
+    plot.start()
+
+
+if __name__=='__main__':
+    main()

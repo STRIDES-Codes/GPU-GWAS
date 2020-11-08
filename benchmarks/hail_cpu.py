2e7e68b31c48bd78f15da342eb91060ecdcfed5d
Joyjit Daw <jdaw@nvidia.com>
2020-11-07 22:13:17 -0500

    [workflow] update workflow script

--- benchmarks/hail_cpu.py
+++ benchmarks/hail_cpu.py
@@ -12,12 +12,14 @@ parser.add_argument('--annotation_path', default = './data/1kg_annotations.txt')
 parser.add_argument('--workdir', default = './temp/')
 args = parser.parse_args()
 
-t0 = time.time()
 
 hl.import_vcf(args.vcf_path).write(args.workdir + 'hail.mt', overwrite=True)
 mt = hl.read_matrix_table(args.workdir + 'hail.mt')
 table = (hl.import_table(args.annotation_path, impute=True)
      .key_by('Sample'))
+
+# Start benchmarking after I/O
+t0 = time.time()
 mt = mt.annotate_cols(pheno = table[mt.s])
 mt = hl.sample_qc(mt)
 mt = mt.filter_cols((mt.sample_qc.dp_stats.mean >= 4) & (mt.sample_qc.call_rate >= 0.97))
@@ -36,6 +38,5 @@ gwas = hl.linear_regression_rows(y=mt.pheno.CaffeineConsumption,
                              x=mt.GT.n_alt_alleles(),
                              covariates=[1.0])
 p = hl.plot.manhattan(gwas.p_value)
-
 
 print('Time Elapsed: {}'.format(time.time()- t0))

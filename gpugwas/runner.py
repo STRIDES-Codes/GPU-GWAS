99235c5d1e78ec55075294cbff141baeb75e8f0a
Vibhu Jawa <vibhujawa@gmail.com>
2020-11-08 11:03:40 -0800

    Update runner.py

--- gpugwas/runner.py
+++ gpugwas/runner.py
@@ -7,13 +7,15 @@ import cudf
 
 def run_gwas(phenotypes_df, phenotype_col, feature_cols, algorithm):
     p_value_dict = defaultdict(list)
+    matrix = cp.array(phenotypes_df[feature_cols].as_gpu_matrix()).astype(cp.float32)
+    y = phenotypes_df[phenotype_col].values.astype(cp.float64)
+    del phenotypes_df
+
     for i, f in enumerate(feature_cols):
         model  = algorithm()
-        feature_columns = [f]
-        X = cp.array(phenotypes_df[feature_columns].as_gpu_matrix()).astype(cp.float64)
-        model.fit(X,phenotypes_df[phenotype_col].values.astype(cp.float64))
-        
-        for p_val,coef,f in zip(model.p_values[1:],model.coefficients[1:],feature_columns):
+        X = cp.expand_dims(matrix[:,i],1)
+        model.fit(X,y)
+        for p_val,coef in zip(model.p_values[1:],model.coefficients[1:]):
             #print(f'Feature:{f} p_value:{p_val}  coef:{coef}')
             p_value_dict["feature"].append(i)
             p_value_dict["p_value"].append(p_val)

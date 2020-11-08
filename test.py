ac2be4b324512bd8a910d4a21e538d5d481518ec
Ayush Dattagupta <ayushdg95@gmail.com>
2020-11-08 03:03:24 +0000

    Add alternate method to transform vcf_df

--- test.py
+++ test.py
@@ -11,4 +11,26 @@ print("Test loading annotation file")
 ann_df = gwasio._load_annotations("data/1kg_annotations.txt")
 print(ann_df)
 
+# Test loading VCF (Method 2)
+print("Test loading VCF to DF (Method 2)")
+vcf_df_2 = gwasio._load_vcf_variantworks(
+    "data/test.vcf", info_keys=["AC", "AF"], format_keys=["GT"]
+)
+vcf_df_2 = gwasio._transform_df(
+    vcf_df_2,
+    sample_key_cols=list(vcf_df_2.columns[14:]),
+    common_key_cols=list(vcf_df_2.columns[7:14]),
+    common_cols=list(vcf_df_2.columns[0:7]),
+    drop_cols=[
+        "id",
+        "variant_type",
+        "AC-1",
+        "AC-2",
+        "AF-1",
+        "AF-2",
+        "end_pos",
+    ],
+)
+print(vcf_df_2)
+
 print("===== TEST PASSED ====")
